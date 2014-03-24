9dbe40e468d819bdb59fd2eab095de4df70d72ec
Charlie Young <charlieyoung@KISSmetrics-Mac-4.local>
2014-03-23 21:24:46 -0700

    prevent default added to second form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
     $.post('/user', data)
   })
 })
+
+$(document).ready(function(){
+  $('#updateRating').on('submit', function(e){
+    e.preventDefault();
+    var data = $(this).serialize()
+    $.post('/game', data)
+  })
+})
